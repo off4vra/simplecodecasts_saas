2d6f8257e30df8ea859981be58ec2d42f391e194
off4vra <off4vra@gmail.com>
2015-05-12 12:11:24 +0000

    Added mailer and view

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,11 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'off4vra@gmail.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    
+    mail(from: email, subject: 'Contact Form Message. Dev Match.')
+  end
+end
